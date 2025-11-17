c9e58425de9f7fa00b21ad04682c51bef1c86aa4
somelamedude-git <cozydeveloper843@gmail.com>
2025-11-18 04:13:21 +0530

    deletion

--- frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './pages/App.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
